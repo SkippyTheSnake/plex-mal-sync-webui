45f8a820d4fa5364e69128a813583e5059c036b7
Reid Mason <maddogshain132@gmail.com>
2019-09-25 17:00:13 +0100

    Added removal of default nginx config

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 python3.7 createSetupfiles.py
+rm /etc/nginx/sites-enabled/default
 cp setupfiles/plex-mal-sync-webui.conf /etc/supervisor/conf.d/plex-mal-sync-webui.conf
 cp setupfiles/plex-mal-sync-webui /etc/nginx/sites-available/plex-mal-sync-webui
 python3.7 -m venv venv
