43fab34afb0164ef0734f70ff05f5aed3515bca8
Reid Mason <maddogshain132@gmail.com>
2019-09-25 17:04:29 +0100

    Fixed typo in filepath

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 python3.7 createSetupfiles.py
 rm /etc/nginx/sites-enabled/default
 cp setupfiles/plex-mal-sync-webui.conf /etc/supervisor/conf.d/plex-mal-sync-webui.conf
-cp setupfiles/plex-mal-sync-webui /etc/nginx/sites-available/plex-mal-sync-webui
+cp setupfiles/plex-mal-sync-webui /etc/nginx/sites-enabled/plex-mal-sync-webui
 python3.7 -m venv venv
 venv/bin/pip3.7 install -r requirements.txt
 systemctl restart nginx
