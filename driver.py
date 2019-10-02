a01ecd5162a1b842c3e0ac99a92215424d9d703f
Reid Mason <maddogshain132@gmail.com>
2019-10-02 08:07:11 +0100

    Added headless flag back

--- driver.py
+++ driver.py
@@ -22,7 +22,7 @@ class Driver:
         log(f"Starting web driver", Fore.CYAN)
         # Setup chrome options
         chrome_options = webdriver.ChromeOptions()
-        # chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
 
